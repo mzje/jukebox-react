4a0f2648ee4a4ef4e22d7096810f12801135406f
Paul Sturgess <paul@kyanmedia.com>
2016-04-29 13:34:37 +0100

    First changes to implement Flux

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
-var fs = require('fs');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
 var watchify = require('watchify');
 var assign = require('lodash.assign');
 
@@ -26,5 +27,8 @@ function bundle() {
   	.bundle()
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
     .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./build'));
 }
