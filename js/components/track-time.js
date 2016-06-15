26cc732d1adedc0a9eb82512f0230241c9c37c68
Adam Lofting <contactme@adamlofting.com>
2016-06-15 12:23:49 +0100

    Basic progress bar

--- js/components/track-time.js
+++ js/components/track-time.js
@@ -11,6 +11,9 @@ class TrackTime extends React.Component {
   contentHTML(duration, time) {
     return(
       <div>
+        <div className='progressBarContainer'>
+          <div className='progressBarContent' style={{ 'width': this.percentage_played(time, duration) + '%' }}></div>
+        </div>
         <p>{this.seconds_to_time(time)} ({ duration })</p>
       </div>
     );
@@ -23,6 +26,17 @@ class TrackTime extends React.Component {
       return number.length<2 ? '0'+number : number;
     }
     return padded(Math.floor(minutes)) + ':' + padded(seconds%60);
+  }
+
+  duration_to_seconds(duration) {
+    let parts = duration.split(':')
+    let mins = parseInt(parts[0])
+    let seconds = parseInt(parts[1])
+    return (mins * 60) + seconds
+  }
+
+  percentage_played(time, duration) {
+    return Math.floor(time / this.duration_to_seconds(duration) * 100)
   }
 
   componentDidMount() {
@@ -38,12 +52,12 @@ class TrackTime extends React.Component {
   componentWillReceiveProps(nextProps) {
       // If a new track is about to start, the time is reset to 0
       if (nextProps.time < 2) {
-        this.state.time = nextProps.time;
+        this.state.time = nextProps.time
       }
 
       // Keep in sync when we get an UPDATE_TIME event
       if (this.props.time !== nextProps.time) {
-        this.state.time = nextProps.time;
+        this.state.time = nextProps.time
       }
   }
 
