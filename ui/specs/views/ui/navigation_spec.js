c2e523eac0faa532726720bff61abf783a407bcd
Paul Sturgess <paul@kyanmedia.com>
2016-06-17 14:13:52 +0100

    Fix Navigation spec

--- ui/specs/views/ui/navigation_spec.js
+++ ui/specs/views/ui/navigation_spec.js
@@ -4,25 +4,33 @@ import Navigation from './../../../src/views/ui/navigation';
 import { Link } from 'react-router'
 
 describe('Navigation', () => {
-  let instance;
+   let instance;
+   let Wrapper = React.createClass({
+     render: function() {
+       return (<div>{this.props.children}</div>);
+     }
+   });
 
-  describe('render', () => {
-    beforeEach(() => {
-      instance = TestUtils.renderIntoDocument(<Navigation />);
-    });
-    it('returns 2 links', () => {
-      let links = TestUtils.scryRenderedDOMComponentsWithTag(instance, 'a')
-      expect(links.length).toEqual(2);
-    });
+  beforeEach(() => {
+    instance = TestUtils.renderIntoDocument(
+      <Wrapper>
+        <Navigation />
+      </Wrapper>
+    )
+  });
 
-    it('returns a link to the root', () => {
-      let links = TestUtils.scryRenderedComponentsWithType(instance, Link)
-      expect(links[0].props.to).toEqual('/')
-    });
+  it('returns 2 links', () => {
+    let links = TestUtils.scryRenderedDOMComponentsWithTag(instance, 'a')
+    expect(links.length).toEqual(2);
+  });
 
-    it('returns a link to the account', () => {
-      let links = TestUtils.scryRenderedComponentsWithType(instance, Link)
-      expect(links[1].props.to).toEqual('/account')
-    });
+  it('returns a link to the root', () => {
+    let links = TestUtils.scryRenderedComponentsWithType(instance, Link)
+    expect(links[0].props.to).toEqual('/')
+  });
+
+  it('returns a link to the account', () => {
+    let links = TestUtils.scryRenderedComponentsWithType(instance, Link)
+    expect(links[1].props.to).toEqual('/account')
   });
 });
