88c1d42162ebf76752be47f416012726d744b900
Paul Sturgess <paul@kyanmedia.com>
2016-06-17 14:42:34 +0100

    Fix listing for vote

--- ui/src/components/vote.js
+++ ui/src/components/vote.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import Actions from '../actions/actions';
 
 class Vote extends React.Component {
+  static propTypes = {
+    userId: React.PropTypes.number,
+    track: React.PropTypes.object
+  }
+
   static contextTypes = {
     jukebox: React.PropTypes.object
   }
 
-  voteUp(event) {
+  voteUp = (event) => {
     event.preventDefault();
     this.context.jukebox.vote(this.props.userId, this.props.track, 1);
   }
 
   voteHTML() {
     return (
-      <a href='#' onClick={ this.voteUp.bind(this) }>Upvote</a>
+      <a href="#" onClick={this.voteUp}>Upvote</a>
     );
   }
 
   render() {
+    let html = null;
     if (this.props.track && this.props.userId) {
-      return this.voteHTML()
-    } else {
-      return null;
-    };
+      html = this.voteHTML();
+    }
+    return html;
   }
 }
 
