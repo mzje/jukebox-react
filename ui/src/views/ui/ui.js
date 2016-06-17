863e096e289711e921769bdcaf22d1d367b0848a
Paul Sturgess <paul@kyanmedia.com>
2016-06-17 15:56:45 +0100

    Fix some of the ui linting issues

--- ui/src/views/ui/ui.js
+++ ui/src/views/ui/ui.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import Dispatcher from './../../dispatcher/dispatcher';
-import Immutable from 'immutable'
 import Store from './../../stores/store';
 import Jukebox from './../../utils/jukebox';
 import SidePanel from './side-panel';
 import Header from './header';
 
 class UI extends React.Component {
+  static childContextTypes = {
+    jukebox: React.PropTypes.object
+  }
+
   constructor(props) {
     super(props);
-    let store = Store
+    const store = Store;
     this.state = {
       jukebox: new Jukebox(),
       store: store,
       storeData: store.currentState()
     };
-  }
-
-  static childContextTypes = {
-    jukebox: React.PropTypes.object
   }
 
   getChildContext = () => {
@@ -28,43 +26,43 @@ class UI extends React.Component {
   }
 
   componentDidMount() {
-    this.state.store.addChangeListener(this._onChange.bind(this));
+    this.state.store.addChangeListener(this._onChange);
     this.state.jukebox.openConnection();
   }
 
   componentWillUnmount() {
-    this.state.store.removeChangeListener(this._onChange.bind(this));
+    this.state.store.removeChangeListener(this._onChange);
   }
 
   sidePanelHTML(track, userId, time) {
-    return(
+    return (
       <SidePanel track={track} userId={userId} time={time} />
-    )
+    );
   }
 
   headerHTML(connection) {
-    return <Header connection={connection} />
+    return <Header connection={connection} />;
   }
 
   /**
    * Event handler for 'change' events coming from the Store
    */
-  _onChange() {
+  _onChange = () => {
     this.setState({
       storeData: this.state.store.currentState()
     });
   }
 
   render() {
-    let track = this.state.storeData.get('track');
-    let userId = this.state.storeData.get('user_id');
-    let time = this.state.storeData.get('time');
-    let connection = this.state.storeData.get('connection');
+    const track = this.state.storeData.get('track');
+    const userId = this.state.storeData.get('user_id');
+    const time = this.state.storeData.get('time');
+    const connection = this.state.storeData.get('connection');
     return (
       <div>
-        { this.headerHTML(connection) }
-        { this.sidePanelHTML(track, userId, time) }
-        { this.props.children }
+        {this.headerHTML(connection)}
+        {this.sidePanelHTML(track, userId, time)}
+        {this.props.children}
       </div>
     );
   }
